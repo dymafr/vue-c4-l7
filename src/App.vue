5008a76de6d11884e97416671d2727781b973d80
dymafr <erwan1309@hotmail.com>
2022-02-22 13:12:10 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,50 @@
 <template>
-  <p>
-    Posez une question (réponse oui / non):
-    <input v-model="question" />
-  </p>
-  <p>Réponse : {{ reponse.res }}</p>
-  <img :src="reponse.image" />
+  <div>
+    <label for="quantity">Nombre de livres : </label>
+    <input id="quantity" type="number" v-model="produit.quantite" />
+  </div>
+
+  <div>
+    <label for="prix">Prix : </label
+    ><input id="prix" type="number" v-model="produit.prix" />
+  </div>
+  <h2>Prix total HT : {{ totalPrixHT }}€</h2>
+  <h2>Prix total TTC : {{ totalPrixTTC }}€</h2>
+  <h3>Modifications : {{ produit.nbrModifs }}</h3>
 </template>
 
 <script setup lang="ts">
-import { ref, watch, reactive } from 'vue';
+import { computed, reactive, watch, watchEffect } from 'vue';
 
-const question = ref('');
-const reponse = reactive({
-  res: 'En attente de votre question...',
-  image: '',
+interface Produit {
+  quantite: number;
+  prix: number;
+  nom: string;
+  nbrModifs: number;
+  derniereModif?: number;
+}
+
+const produit = reactive({
+  quantite: 3,
+  prix: 10,
+  nom: 'livre',
+  nbrModifs: 0,
 });
 
-watch(question, async (newQuestion, oldQuestion) => {
-  if (newQuestion.includes('?')) {
-    reponse.res = 'Requête en cours...';
-    try {
-      const res = await fetch('https://yesno.wtf/api');
-      const resJSON = await res.json();
-      reponse.res = resJSON.answer;
-      reponse.image = resJSON.image;
-    } catch (error) {
-      reponse.res = `Erreur : ${error}`;
-    }
+const totalPrixHT = computed(() => produit.quantite * produit.prix);
+const totalPrixTTC = computed(() => produit.quantite * produit.prix * 1.2);
+
+watch(
+  () => [produit.quantite, produit.prix],
+  (nouvelleVal, ancienneVal) => {
+    console.log(nouvelleVal, ancienneVal);
+    produit.nbrModifs++;
   }
+);
+
+watchEffect(() => {
+  produit.derniereModif = Date.now();
+  console.log(produit);
 });
 </script>
 
